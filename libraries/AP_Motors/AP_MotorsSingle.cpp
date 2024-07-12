35c70a2c9b02120b15921af488412f14e2f20540
MapleAviation <christoph.busch@maple-aviation.com>
2024-07-12 13:16:29 +0200

    Update AP_MotorsSingle.cpp

--- libraries/AP_Motors/AP_MotorsSingle.cpp
+++ libraries/AP_Motors/AP_MotorsSingle.cpp
@@ -174,9 +174,9 @@ void AP_MotorsSingle::output_armed_stabilizing()
 
     // combine roll, pitch and yaw on each actuator
     // front servo
-    actuator[0] = rp_scale * roll_thrust + yaw_thrust;
+    actuator[0] = rp_scale * roll_thrust - yaw_thrust;
     // right servo
-    actuator[1] = rp_scale * pitch_thrust - yaw_thrust;
+    actuator[1] = rp_scale * pitch_thrust + yaw_thrust;
     // rear servo
     actuator[2] = -rp_scale * roll_thrust + yaw_thrust;
     // left servo
